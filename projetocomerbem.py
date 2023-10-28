7228e66e2c24ccb8761d804310008a9aa722f9e6
Nthldev <nthlevis@gmail.com>
2023-10-28 20:38:10 -0300

    codigo criado ja com varias considerações

--- projetocomerbem.py
+++ projetocomerbem.py
@@ -1,2 +1,56 @@
+from data import*
+
 print("Bem vindo ao Comer Bem!")
 print("O app com a seleção dos melhores restaurantes do Rio.")
+
+def mostrar_opcoes_culinaria():
+    alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "W", "Y", "Z"]
+    letra = input("Digite a letra inicial da culinária desejada: ").upper()
+    if letra in alfabeto:
+        opcoes = [culinaria for culinaria in restaurantes if culinaria.startswith(letra)]
+        print("Opções de culinárias disponíveis:")
+        for opcao in opcoes:
+            print(opcao)
+    else:
+        print("Perdão, não reconheço a letra digitada")
+        mostrar_opcoes_culinaria()
+
+def mostrar_restaurantes(culinaria):
+    if culinaria in restaurantes:
+        print()
+        print(f"Restaurantes de culinária {culinaria}:")
+        for restaurante in restaurantes[culinaria]:
+            print(f"Nome: {restaurante['nome']}")
+            print(f"Endereço: {restaurante['endereço']}")
+            print(f"Telefone: {restaurante['telefone']}")
+            print(f"Avaliação: {restaurante['avaliação']}")
+            print()
+        repetir = input("Deseja realizar uma nova busca? Digite s para sim ou n para encerrar").lower()
+        if repetir == "n":
+            print("encerrando programa")
+            exit()
+        elif repetir == "s":
+            mostrar_opcoes_culinaria()
+        else:
+            print("Perdão, não entendi sua resposta. Seguem novamente as opções culinárias")
+            mostrar_opcoes_culinaria()
+    else:
+        print(f"Nenhum restaurante encontrado para a culinária {culinaria}")
+        repetir = input("Deseja realizar uma nova busca? Digite s para sim ou n para encerrar").lower()
+        if repetir == "n":
+            print("encerrando programa")
+            exit()
+        elif repetir == "s":
+            mostrar_opcoes_culinaria()
+        else:
+            print("Perdão, não entendi sua resposta. Seguem novamente as opções culinárias")
+            mostrar_opcoes_culinaria()
+
+
+mostrar_opcoes_culinaria()
+
+culinaria_escolhida = input("Escolha uma das opções de culinária: ").title()
+
+mostrar_restaurantes(culinaria_escolhida)
+
+
